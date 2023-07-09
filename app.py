367e2ac9d004408e2fcdc901d874d65e50801234
Cayla Valentine <123324519+caylavalentine@users.noreply.github.com>
2023-07-09 11:10:57 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,54 @@
 # Import the dependencies.
-
+from flask import Flask
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 
 #################################################
 # Database Setup
 #################################################
 
-
 # reflect an existing database into a new model
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
+Base = automap_base()
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
+Base.classes.keys()
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    return """
+        /api/v1.0/precipitation \n
+        /api/v1.0/stations \n
+        /api/v1.0/tobs \n
+        /api/v1.0/<start> \n
+        /api/v1.0/<start>/<end> \n
+    """
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
